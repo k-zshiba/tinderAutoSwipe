924714c6216ac116ba30fe4502c75870f0f26a95
k-zshiba <kou480929@gmail.com>
2021-01-26 23:04:50 +0900

    create randomSwipe.js

---
+++ randomSwipe.js
@@ -0,0 +1,15 @@
+const nope_button = document.getElementsByClassName("button")[1];
+const like_button = document.getElementsByClassName("button")[3];
+
+
+
+function judgeAge() {
+  let str_age = document.getElementsByClassName("As(b)")[5].textContent;
+  let age = Number(str_age);
+  if (age >= younger_age && age <= older_age) {
+    like_button.click();
+  }else {
+    nope_button.click();
+  }
+}
+setInterval(judgeAge,1000);
