c656d9012e9ae44aa70743a77e70472f7bbeb7a5
k-zshiba <kou480929@gmail.com>
2021-01-26 23:24:50 +0900

    create function getRandomInt

--- randomSwipe.js
+++ randomSwipe.js
@@ -1,15 +1,21 @@
 const nope_button = document.getElementsByClassName("button")[1];
 const like_button = document.getElementsByClassName("button")[3];
 
+const time_interval_ms = 1000;
 
+const like_ratio = 50;
 
-function judgeAge() {
-  let str_age = document.getElementsByClassName("As(b)")[5].textContent;
-  let age = Number(str_age);
-  if (age >= younger_age && age <= older_age) {
+function getRandomInt() {
+  min = 0;
+  max = 100;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function likeOrNope() {
+  if (getRandomInt(like_ratio) <= like_ratio) {
     like_button.click();
   }else {
     nope_button.click();
   }
 }
-setInterval(judgeAge,1000);
+setInterval(likeOrNope,time_interval_ms);
